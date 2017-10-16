2f0308d7e0c8ea4f43618be2758992ba676f6eb6
gabefoley <gabriel.foley@uqconnect.edu.au>
2017-10-16 12:44:50 +1000

    Added instructions to notebook. Added gitignore

--- 1kp_taxonomy_map.py
+++
@@ -1,8 +0,0 @@
-from Bio import SeqIO
-
-records = SeqIO.parse("files/CYP711_uncleaned.fasta", "fasta")
-
-for record in records:
-    print (record)
-    print (record.name)
-    print (record.description)
